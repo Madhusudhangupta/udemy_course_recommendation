f857dc0777faeda772e582fc4919d98f35fb9a60
Madhusudhan Gupta <madhusudhangupta18@gmail.com>
2023-03-27 18:51:30 +0530

    Added files

---
+++ dashboard.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+def getvaluecounts(df):
+    return dict(df['subject'].value_counts())
+
+def getlevelcount(df):
+    return dict(list(df.groupby(['level'])['num_subscribers'].count().items())[1:])
+
+def getsubjectsperlevel(df):
+    ans = list(dict(df.groupby(['subject'])['level'].value_counts()).keys())
+    all_labels = [ans[i][0] + '_' + ans[i][1] for i in range(len(ans))]
+    ansvalues = list(dict(df.groupby(['subject'])['level'].value_counts()).values())
+    complete_dict = dict(zip(all_labels, ansvalues))
+    return complete_dict
+
+def yearwiseprofit(df):
+    df['price'] = df['price'].astype(str).str.replace('TRUE|Free', '0')
+    df['price'] = df['price'].astype('float')
+    df['profit'] = df['price'] * df['num_subscribers']
+    df['published_date'] = df['published_timestamp'].apply(lambda x: x.split('T')[0])
+    df = df.drop(df.index[2066])
+    df['published_date'] = pd.to_datetime(df['published_date'], format="%Y-%m-%d")
+    df['Year'] = df['published_date'].dt.year
+    df['Month'] = df['published_date'].dt.month
+    df['Day'] = df['published_date'].dt.day
+    df['Month_name'] = df['published_date'].dt.month_name()
+    
+    profitmap = dict(df.groupby(['Year'])['profit'].sum())
+    subscribersmap = dict(df.groupby(['Year'])['num_subscribers'].sum())
+    profitmonthwise = dict(df.groupby(['Month_name'])['profit'].sum())
+    monthwisesub = dict(df.groupby(['Month_name'])['num_subscribers'].sum())
+    return profitmap, subscribersmap,profitmonthwise,monthwisesub
